b008e319913f11510bcf329a0c2d6665396b7335
Luis Barrantes Mora <luis1456.3@gmail.com>
2024-08-09 23:00:07 -0600

    First commit

---
+++ NZWalks/NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] names = new string[] { "Jhon", "Jane", "Mark", "Emily", "David" };
+            return Ok(names);
+        }
+    }
+}
